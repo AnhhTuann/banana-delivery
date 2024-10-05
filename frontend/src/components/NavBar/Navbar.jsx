1ba6f130685aa315f27f9c10bc5ac40c563a3ca3
AnhhTuann <tuannguyen25101999@gmail.com>
2024-10-05 19:36:22 +0700

    install scss to project

---
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./NavBar.css";
+
+const Navbar = () => {
+  return <div className="navbar"></div>;
+};
+
+export default Navbar;
