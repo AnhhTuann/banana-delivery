67c4b4cb87151ab73573201c978b2988a04ce68a
AnhhTuann <tuannguyen25101999@gmail.com>
2024-10-07 11:11:20 +0700

    add header component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,11 @@
+import Header from "../../components/Header/Header";
 import "./Home.css";
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
